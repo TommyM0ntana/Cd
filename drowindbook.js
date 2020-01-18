01ae4d169e988c5c0c5920866dcafd50881f1de6
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-18 22:10:21 +0100

    add drowingbook challenge

---
+++ drowindbook.js
@@ -0,0 +1,24 @@
+function drawingBook(n, p) {
+    // Reduce the problem, if odd consider it same as even.
+    if (n % 2 !== 0) n--;
+  
+    let beginningCountPages = 0;
+    for (let i = 1; i <= p; i++) {
+      if (i % 2 === 0) beginningCountPages++;
+    }
+  
+    let endCountPages = 0;
+    for (let i = n; i >= p; i--) {
+      if (i % 2 !== 0) endCountPages++;
+    }
+  
+    return Math.min(beginningCountPages, endCountPages);
+  
+    // let pg = Math.floor(p / 2);
+    // let t = Math.floor(n / 2);
+    // let turns = Math.min(pg, t-pg);
+    // return turns;
+  }
+  
+  console.log(drawingBook(6, 2)) // 1
+  console.log(drawingBook(5, 4)) // 0
