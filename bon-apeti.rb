9a77b52f88f2ec1c0bd854033713a201ff9eae20
Tommym0ntana <tommy.montana.harwood@gmail.com>
2020-01-28 12:34:14 +0100

    bon appetit chal

---
+++ bon-apeti.rb
@@ -0,0 +1,12 @@
+
+def bon_appetit(bill, k, b)
+    total = bill.reduce(:+)
+    ana = (total - bill[k]) / 2
+    print b > ana ? b - ana : 'Bon Appetit'
+  end
+  
+  def solve
+    bon_appetit([3, 10, 2, 9], 1, 12) # 5
+    bon_appetit([3, 10, 2, 9], 1, 7) # Bon Appetit
+  end
+  
