2fa0ecd330dd9263687bb052c450fee419245285
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-02-15 19:34:12 +0100

    add insertion sort

--- solve.rb
+++ solve.rb
@@ -1,21 +1,40 @@
-#!/usr/bin/env ruby
-# https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/fizzbuzz
+# frozen_string_literal: true
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-(1..100).map{|k| puts (k%15==0 && 'FizzBuzz')||(k%3==0 && 'Fizz')||(k%5==0 && 'Buzz')||k}
+def sort(n, arr)
+  puts arr.to_s
+  puts
+  to_sort = arr[n - 1]
+  (0...n).reverse_each do |i|
+    # puts arr.to_s
+    # puts 'arr[i]: ' + arr[i].to_s
+    # puts 'arr[i-1]: ' + arr[i-1].to_s
 
-# a=*(1..100)
-# a.each do |i|
-#   *i += 100
-#   # print i.to_s + ' '
-#   # if i%15==0
-#   #   puts "FizzBuzz"
-#   # elsif i%5==0
-#   #     puts "Buzz"
-#   # elsif i%3==0
-#   #     puts "Fizz"
-#   # else
-#   #     puts i
-#   # end
-# end
-# puts a
+    arr[i] = arr[i - 1]
+    if i.zero? || arr[i - 1] <= to_sort
+      arr[i] = to_sort
+      puts arr.join(' ')
+      break
+    end
+    puts arr.join(' ')
+  end
+end
+
+def solve
+  # sort(5, [1, 2, 4, 5, 3])
+  # # 1 2 4 5 5
+  # # 1 2 4 4 5
+  # # 1 2 3 4 5
+
+  # sort(5, [2, 3, 4, 5, 1])
+  # # 2, 3, 4, 5, 5
+  # # 2, 3, 4, 4, 5
+  # # 2, 3, 3, 4, 5
+  # # 2, 2, 3, 4, 5
+  # # 1, 2, 3, 4, 5
+
+  sort(5, [2, 4, 6, 8, 3])
+  # 2 4 6 8 8
+  # 2 4 6 6 8
+  # 2 4 4 6 8
+  # 2 3 4 6 8
+end
